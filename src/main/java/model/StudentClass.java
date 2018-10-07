bdafb2f1952dc5dcaac40ae88f5b6f12cf4889c0
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2018-10-07 15:04:44 +0200

    student class

---
+++ src/main/java/model/StudentClass.java
@@ -0,0 +1,56 @@
+package model;
+
+import java.util.ArrayList;
+
+public class StudentClass {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String programme;
+    private ArrayList<String> courses;
+
+    //constructor
+    public StudentClass() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProgramme() {
+        return programme;
+    }
+
+    public void setProgramme(String program) {
+        this.programme = program;
+    }
+
+    public ArrayList<String> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(ArrayList<String> courses) {
+        this.courses = courses;
+    }
+}
