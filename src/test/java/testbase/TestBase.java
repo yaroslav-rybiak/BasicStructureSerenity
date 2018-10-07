8df45ab0a19fdf264c03101318a10df289e882b0
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2018-10-07 15:04:59 +0200

    base test class with init method

---
+++ src/test/java/testbase/TestBase.java
@@ -0,0 +1,12 @@
+package testbase;
+
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+public class TestBase {
+
+    @BeforeClass
+    public static void init() {
+        RestAssured.baseURI = "http://localhost:8080/student";
+    }
+}
