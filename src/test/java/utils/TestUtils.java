c449c02b117a976a0d1d6c20561d7639ac32bf99
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2018-10-07 15:04:28 +0200

    method that generates random string

---
+++ src/test/java/utils/TestUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+import java.util.Random;
+
+public class TestUtils {
+
+    public static String getRandomValue() {
+
+        Random random = new Random();
+        int randomInt = random.nextInt(10000);
+        return Integer.toString(randomInt);
+    }
+}
