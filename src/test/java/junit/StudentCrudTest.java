79ad04a85cac950fbc61d49bc4bd1017448afe87
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2018-10-08 22:32:46 +0200

    ordering tests, creating student id that is avaialbe from all tests. creating a student and checking that student was created

--- src/test/java/junit/StudentCrudTest.java
+++ src/test/java/junit/StudentCrudTest.java
@@ -1,36 +1,40 @@
 package junit;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import model.StudentClass;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Pending;
 import net.thucydides.core.annotations.Title;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import testbase.TestBase;
-
 import java.util.ArrayList;
-
 import static utils.TestUtils.getRandomValue;
 
 @RunWith(SerenityRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StudentCrudTest extends TestBase {
 
-    @Title("Create a new student")
-    @Test
-    public void createStudent() {
+    private static String studentId;
 
+    @Title("Add a new student")
+    @Test
+    public void test001() {
         ArrayList<String> courses = new ArrayList<>();
         courses.add("Java");
         courses.add("Selenium");
-
         StudentClass student = new StudentClass();
         student.setFirstName("Aaa" + getRandomValue());
         student.setLastName("Bbb" + getRandomValue());
-        student.setEmail("aaa" + getRandomValue() + "@gmail.com");
+        student.setEmail(student.getFirstName() + "@gmail.com");
         student.setProgramme("QA");
         student.setCourses(courses);
-        SerenityRest.given()
+        studentId = SerenityRest.rest()
+                .given()
                 .contentType(ContentType.JSON)
                 .log()
                 .all()
@@ -40,6 +44,52 @@ public class StudentCrudTest extends TestBase {
                 .then()
                 .log()
                 .all()
-                .statusCode(201);
+                .statusCode(201)
+                .extract()
+                .response()
+                .getHeader("Location")
+                .replace(RestAssured.baseURI + "/", "");
+    }
+
+    @Title("Verify that student was added")
+    @Test
+    public void test002() {
+        String studentUrl = RestAssured.baseURI + "/" + studentId;
+        SerenityRest.rest()
+                .given()
+                .when()
+                .get(studentUrl)
+                .then()
+                .log()
+                .all()
+                .statusCode(200);
+    }
+
+    @Pending
+    @Title("Update student data")
+    @Test
+    public void test003() {
+
+    }
+
+    @Pending
+    @Title("Verify that student data was updated")
+    @Test
+    public void test004() {
+
+    }
+
+    @Pending
+    @Title("Delete a student")
+    @Test
+    public void test005() {
+
+    }
+
+    @Pending
+    @Title("Verify that student was deleted")
+    @Test
+    public void test006() {
+
     }
 }
